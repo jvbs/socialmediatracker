4efd2351b3dae976b2a36ad5da12412853d0e89c
João Vitor Barbosa Souza <jvbs98@hotmail.com>
2020-12-08 01:24:54 -0300

    gerado template layout plugin

--- includes/socialmediatracker-class.php
+++ includes/socialmediatracker-class.php
@@ -29,7 +29,7 @@ class SMT_Subs_Widget extends WP_Widget {
 			echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ) . $args['after_title'];
 		}
     // echo esc_html__( 'Hello, World!', 'smt_domain' );
-    echo '<div class="g-ytsubscribe" data-channel="pewdiepie" data-layout="full" data-count="default"></div>';
+    echo '<div class="g-ytsubscribe" data-channel="'.$instance['canal'].'" data-layout="'.$instance['layout'].'" data-count="default"></div>';
 
 		echo $args['after_widget'];
 	}
@@ -43,11 +43,33 @@ class SMT_Subs_Widget extends WP_Widget {
 	 */
 	public function form( $instance ) {
 		$title = ! empty( $instance['title'] ) ? $instance['title'] : esc_html__( 'Social Media Tracker - YT', 'smt_domain' );
-		$canal = ! empty( $instance['title'] ) ? $instance['title'] : esc_html__( 'Social Media Tracker - YT', 'smt_domain' );
+		$canal = ! empty( $instance['canal'] ) ? $instance['canal'] : esc_html__( 'pewdiepie', 'smt_domain' );
+		$layout = ! empty( $instance['layout'] ) ? $instance['layout'] : esc_html__( 'full', 'smt_domain' );
 		?>
 		<p>
 		  <label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php esc_attr_e( 'Title:', 'smt_domain' ); ?></label> 
 		  <input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
+		</p>
+
+		<p>
+		  <label for="<?php echo esc_attr( $this->get_field_id( 'canal' ) ); ?>"><?php esc_attr_e( 'Canal:', 'smt_domain' ); ?></label> 
+		  <input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'canal' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'canal' ) ); ?>" type="text" value="<?php echo esc_attr( $canal ); ?>">
+		</p>
+
+		<p>
+		  <label for="<?php echo esc_attr( $this->get_field_id( 'layout' ) ); ?>"><?php esc_attr_e( 'layout:', 'smt_domain' ); ?></label> 
+		  <select 
+          class="widefat" 
+          id="<?php echo esc_attr( $this->get_field_id( 'layout' ) ); ?>" 
+          name="<?php echo esc_attr( $this->get_field_name( 'layout' ) ); ?>">
+          <option value="default" <?php echo ($layout == 'default') ? 'selected' : ''; ?>>
+            Default
+          </option>
+          <option value="full" <?php echo ($layout == 'full') ? 'selected' : ''; ?>>
+            Full
+          </option>
+        </select>
+      </p>
 		</p>
 		<?php 
 	}
@@ -65,6 +87,10 @@ class SMT_Subs_Widget extends WP_Widget {
 	public function update( $new_instance, $old_instance ) {
 		$instance = array();
 		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? sanitize_text_field( $new_instance['title'] ) : '';
+
+		$instance['canal'] = ( ! empty( $new_instance['canal'] ) ) ? sanitize_text_field( $new_instance['canal'] ) : '';
+		
+		$instance['layout'] = ( ! empty( $new_instance['layout'] ) ) ? sanitize_text_field( $new_instance['layout'] ) : '';
 
 		return $instance;
 	}
